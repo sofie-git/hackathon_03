2763ece1f4838365ed3e38b7b59576bb0ca13146
Eva Denys <eva.denys@student.howest.be>
2019-10-23 00:06:31 +0200

    earth texture + inlezen x & y cordinaten

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,22 +1,61 @@
 import "./css/style.css";
 
 {
-  const init = () => {
-    console.log(`START`);
-    const canvas = document.querySelector("#c");
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    const fov = 75;
-    const aspect = 2; // the canvas default
-    const near = 0.1;
-    const far = 5;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
-    const scene = new THREE.Scene();
-    //
-    // earth
+  //setup van uw canvas en camera
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  const fov = 75;
+  const aspect = 2; // canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+  const scene = new THREE.Scene();
+  // texture loader
+  const loader = new THREE.TextureLoader();
+  //
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+  //
+  let x, y;
+  let earths = [];
+  //
+  const drawOnCanvas = () => {
+    // element aarde
+    const earthMaterial = new THREE.MeshBasicMaterial({
+      // photo inladen for texture usache
+      map: loader.load("./assets/img/bak.jpg")
+    });
     const radius = 0.5;
     const geometry = new THREE.DodecahedronBufferGeometry(radius);
-    const earths = [makeInstance(geometry, 0x935d2b, 0)];
+    earths = [makeInstance(geometry, earthMaterial, 0, 0)];
+    //
+    function makeInstance(geometry, material, x, y) {
+      const shape = new THREE.Mesh(geometry, material);
+      scene.add(shape);
+
+      shape.position.x = x;
+      shape.position.y = y;
+      return shape;
+    }
+
+    //updaten van de x en y van earth hetzelfde is als de muis x en y
+    canvas.addEventListener("mousemove", e => {
+      // x = e.clientX;
+      // y = e.clientY;
+      // earths.forEach(earth => {
+      //   earth.position.x = x;
+      //   earth.position.y = y;
+      // });
+      // earths = [makeInstance(geometry, earthMaterial, e.clientX, e.clientY)];
+      // makeInstance(geometry, earthMaterial, e.clientX, e.clientY);
+      console.log(e.clientX, e.clientY);
+    });
     //
     function render(time) {
       time *= 0.001; // convert time to seconds
@@ -39,25 +78,6 @@ import "./css/style.css";
       requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
-
-    {
-      const color = 0xffffff;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(-1, 2, 4);
-      scene.add(light);
-    }
-    //
-    function makeInstance(geometry, color, x) {
-      const material = new THREE.MeshPhongMaterial({ color });
-
-      const shape = new THREE.Mesh(geometry, material);
-      scene.add(shape);
-
-      shape.position.x = x;
-
-      return shape;
-    }
     //
     function resizeRendererToDisplaySize(renderer) {
       const canvas = renderer.domElement;
@@ -70,7 +90,13 @@ import "./css/style.css";
       }
       return needResize;
     }
-    //
   };
+  //
+  const init = () => {
+    console.log(`START`);
+    drawOnCanvas();
+    console.log();
+  };
+  //
   init();
 }
