a158c746935f79fd82c5a7c6fef686568f52c86c
Eva Denys <eva.denys@student.howest.be>
2019-10-23 11:46:42 +0200

    Initiele Phaser setup

---
+++ src/renderer/js/classes/gameobjects/Bullet.js
@@ -0,0 +1,9 @@
+export default class Bullet extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    // super(scene, x, y, `bla`);
+    // //
+    // scene.add.existing(this);
+    // scene.physics.add.existing(this);
+    // //
+  }
+}
